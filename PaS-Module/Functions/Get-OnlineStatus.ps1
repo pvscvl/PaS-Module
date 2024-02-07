06f7f0e6cb3b71a5c6c6ce630a7f204cd3f7a74e
pvscvl <pascalschoofs@icloud.com>
2024-02-07 11:43:48 +0100

    Delete PaS-Module directory

--- PaS-Module/Functions/Get-OnlineStatus.ps1
+++
@@ -1,9 +0,0 @@
-function Get-OnlineStatus {
-	param (
-		[Parameter(Mandatory = $true)]
-		[ValidateNotNullOrEmpty()]
-		[string]$Computer
-	)
-	$ping = Test-Connection -ComputerName $Computer -Count 1 -Quiet
-	return $ping
-}
--- PaS-Module/PaS-Module.psm1
+++
@@ -1,4 +0,0 @@
-$FUNCTIONFILES = Get-ChildItem -Path $PSScriptRoot\Functions\*.ps1
-foreach ($file in $FUNCTIONFILES) {
-    . $file.FullName
-}
