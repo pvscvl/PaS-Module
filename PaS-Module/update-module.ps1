ecbca891147e59be1de5b5b38895189972039167
pvscvl <pascalschoofs@icloud.com>
2024-02-22 15:02:35 +0100

    .

--- PaS-Module/update-module.ps1
+++ PaS-Module/update-module.ps1
@@ -1,19 +1,12 @@
 $SCRIPTDIRECTORY = Split-Path -Parent $MyInvocation.MyCommand.Path
-$SCRIPTDIRECTORY 
-$FUNCTIONSFOLDER = Join-Path -Path $SCRIPTDIRECTORY -ChildPath "functions"
-$FUNCTIONSFOLDER 
+$FUNCTIONSFOLDER = Join-Path -Path $SCRIPTDIRECTORY -ChildPath "Functions"
 $MODULEFILE = Join-Path -Path $SCRIPTDIRECTORY -ChildPath "PaS-Module.psm1"
-$MODULEFILE 
 
-# Get all .ps1 files in the functions folder
-#	$scriptFiles = Get-ChildItem -Path $FUNCTIONSFOLDER -Filter "*.ps1"
+	$scriptFiles = Get-ChildItem -Path $FUNCTIONSFOLDER -Filter "*.ps1"
+	$moduleContent = ""
 
-#	$moduleContent = ""
-
-
-#	foreach ($scriptFile in $scriptFiles) {
-#	$content = Get-Content -Path $scriptFile.FullName -Raw
-#	$moduleContent += $content + [Environment]::NewLine + [Environment]::NewLine
-#	}
-
-#	$moduleContent | Out-File -FilePath $MODULEFILE -Force
+	foreach ($scriptFile in $scriptFiles) {
+		$content = Get-Content -Path $scriptFile.FullName -Raw
+		$moduleContent += $content + [Environment]::NewLine + [Environment]::NewLine
+	}
+$moduleContent | Out-File -FilePath $MODULEFILE -Force
