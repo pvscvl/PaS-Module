db86f29b9648852efc0d0a8684d6c944ca02ad93
pvscvl <pascalschoofs@icloud.com>
2024-02-23 18:01:59 +0100

    .

--- PaS-Module/Functions/201_Test-Credentials.ps1
+++ PaS-Module/Functions/201_Test-Credentials.ps1
@@ -4,28 +4,36 @@ function Test-Credentials {
 		[Parameter(Mandatory=$true)]
 		[string]$UserName
 	)
+	Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+	$DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext('domain')
+	$UserAccount = [System.DirectoryServices.AccountManagement.UserPrincipal]::FindByIdentity($DS, $UserName)
+	if ($UserAccount -ne $null) {
+		if ($null -ne $UserAccount.AccountLockoutTime) {
+		Write-Warning "Account of $UserName is locked."
+		return $null
+		}
+	}
+
 	$SecurePassword = Read-Host "Enter password for $UserName" -AsSecureString
 	if (-not $SecurePassword) {
 		Write-Warning 'Test-ADCredential: Password cannot be empty'
 		return
 	}
-	Add-Type -AssemblyName System.DirectoryServices.AccountManagement
-	$DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext('domain')
-	$user = [System.DirectoryServices.AccountManagement.UserPrincipal]::FindByIdentity($DS, $UserName)
-	if ($user) {
-	#	if ($User.AccountLockoutTime -ne $null) {
-		if ($null -ne $User.AccountLockoutTime) {
-			$User.UnlockAccount()
-			Write-Host "Account of $UserName unlocked."
-			Write-Host "Account of $User unlocked."
-		}
-	}
+
 	$VALIDCRED = $DS.ValidateCredentials($UserName, [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePassword)))
 
 	if ($VALIDCRED) {
 		Write-Output "Credentials for $UserName were correct."
 	} else {
 		Write-Warning "Credentials for $UserName were incorrect."
+		$user = [System.DirectoryServices.AccountManagement.UserPrincipal]::FindByIdentity($DS, $UserName)
+		if ($user) {
+			if ($null -ne $User.AccountLockoutTime) {
+				Write-Warning "Account of $UserName is locked out."
+				$User.UnlockAccount()
+				Write-Host "Account of $UserName unlocked."
+			}
+		}
 	}
 }
 
