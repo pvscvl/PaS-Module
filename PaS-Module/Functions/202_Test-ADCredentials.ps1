09fc67dd32ea0db2a5592961a6a55644faed94a0
pvscvl <pascalschoofs@icloud.com>
2024-02-23 17:22:56 +0100

    .

--- PaS-Module/Functions/202_Test-ADCredentials.ps1
+++ PaS-Module/Functions/202_Test-ADCredentials.ps1
@@ -29,19 +29,40 @@ function Test-ADCredentials {
 		Add-Type -AssemblyName System.DirectoryServices.AccountManagement
 		$DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext('domain')
 		$user = [System.DirectoryServices.AccountManagement.UserPrincipal]::FindByIdentity($DS, $UserName)
-		if ($user) {
-	#		if ($User.AccountLockoutTime -ne $null) {
-			if ($null -ne $User.AccountLockoutTime) {
-				$User.UnlockAccount()
-				Write-Host "Account of $UserName unlocked."
-				Write-Host "Account of $User unlocked."
+
+		$UserAccount = [System.DirectoryServices.AccountManagement.UserPrincipal]::FindByIdentity($DS, $UserName)
+		if ($UserAccount -ne $null) {
+			if ($null -ne $UserAccount.AccountLockoutTime) {
+			Write-Warning "Account of $UserName is locked."
+			return $null
 			}
 		}
+	
+
+
+	#	if ($user) {
+	#		if ($User.AccountLockoutTime -ne $null) {
+	#		if ($null -ne $User.AccountLockoutTime) {
+	#			$User.UnlockAccount()
+	#			Write-Host "Account of $UserName unlocked."
+	#			Write-Host "Account of $User unlocked."
+	#		}
+	#	}
 		$VALIDCRED = $DS.ValidateCredentials($UserName, $Password)
 		if ($VALIDCRED) {
 			Write-Output "Credentials for $UserName were correct."
 		} else {
 			Write-Warning "Credentials for $UserName were incorrect."
+			$UserAccount = [System.DirectoryServices.AccountManagement.UserPrincipal]::FindByIdentity($DS, $UserName)
+
+			if ($UserAccount -ne $null) {
+				if ($null -ne $UserAccount.AccountLockoutTime) {
+					Write-Warning "Account of $UserName is locked."
+					$UserAccount.UnlockAccount()
+					Write-Host "Account of $UserAccount unlocked."
+				}
+			}
+
 		}
 	}
 }
--- PaS-Module/Functions/203_Test-DefaultCredentials.ps1
+++ PaS-Module/Functions/203_Test-DefaultCredentials.ps1
@@ -10,23 +10,44 @@ function Test-DefaultCredentials {
 	$passwordFound = $false
 	Add-Type -AssemblyName System.DirectoryServices.AccountManagement
 	$DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext('domain')
+
+	$UserAccount = [System.DirectoryServices.AccountManagement.UserPrincipal]::FindByIdentity($DS, $UserName)
+	if ($UserAccount -ne $null) {
+		if ($null -ne $UserAccount.AccountLockoutTime) {
+		Write-Warning "Account of $UserName is locked."
+		return $null
+		}
+	}
+
+
 	foreach ($PASSWORD in $PASSWORDS) {
 		$isValid = $DS.ValidateCredentials($UserName, $PASSWORD)
 			if ($isValid) {
-			$PasswordPrefix = $PASSWORD.Substring(0, 3)
-			Write-Warning "${PasswordPrefix}-default password is in use."
-		#	Write-Warning "$PasswordPrefix default password is in use."	
-			$passwordFound = $true
-			break
-		}
-		$User = [System.DirectoryServices.AccountManagement.UserPrincipal]::FindByIdentity($DS, $UserName)
+				$PasswordPrefix = $PASSWORD.Substring(0, 3)
+				Write-Warning "${PasswordPrefix}-default password is in use."
+				$passwordFound = $true
+				break
+			} else {
+				Write-Warning "Credentials for $UserName were incorrect."
+				$UserAccount = [System.DirectoryServices.AccountManagement.UserPrincipal]::FindByIdentity($DS, $UserName)
+	
+				if ($UserAccount -ne $null) {
+					if ($null -ne $UserAccount.AccountLockoutTime) {
+						Write-Warning "Account of $UserName is locked."
+						$UserAccount.UnlockAccount()
+						Write-Host "Account of $UserAccount unlocked."
+					}
+				}
+			}
+	}		
+	#	$User = [System.DirectoryServices.AccountManagement.UserPrincipal]::FindByIdentity($DS, $UserName)
 	#	if ($User.AccountLockoutTime -ne $null) {
-		if ($null -ne $User.AccountLockoutTime) {
-				$User.UnlockAccount()
-				Write-Host "Account of $UserName unlocked."
-				Write-Host "Account of $User unlocked."
-		}
-	}
+	#	if ($null -ne $User.AccountLockoutTime) {
+	#			$User.UnlockAccount()
+	#			Write-Host "Account of $UserName unlocked."
+	#			Write-Host "Account of $User unlocked."
+	#	}
+	
 	if (-not $passwordFound) {
 		Write-Host "$UserName is not using any of the default passwords."
 	}
