1859269d3f5f7b848994eb7bf37600ba9167e176
pvscvl <pascalschoofs@icloud.com>
2024-02-27 06:01:11 +0100

    Update _505_Send-Mail.ps1

--- PaS-Module/Functions/_505_Send-Mail.ps1
+++ PaS-Module/Functions/_505_Send-Mail.ps1
@@ -17,7 +17,8 @@ function Send-Mail {
 	$Body = "Hallo zusammen,`r`n`r`nLorem ipsum dolor sit amet, consectetur adipiscing elit.`r`nSed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`r`n`r`n$(Get-Date -Format "$DateFormatPt1")`r`n$(Get-Date -Format "$DateFormatPt2")`r`n$KW`r`n$(Get-Date -Format "$DateFormatPt3")`r`n`r`n`r`nViele Grüße"
 
 	try {
-		Send-MailMessage -To $To -Subject $Subject -Body $Body -From $From -SmtpServer $SMTPServer -UseSsl -Port $Port -Credential $null -DeliveryNotificationOption OnSuccess
+	#	Send-MailMessage -To $To -Subject $Subject -Body $Body -From $From -SmtpServer $SMTPServer -UseSsl -Port $Port -Credential $null -DeliveryNotificationOption OnSuccess
+  		Send-MailMessage -To $To -Subject $Subject -Body $Body -From $From -SmtpServer $SMTPServer -UseSsl -Port $Port -DeliveryNotificationOption OnSuccess
 		Write-Host "Email sent successfully to $To" -ForegroundColor Green
 	} catch {
 		Write-Host "Failed to send email: $_" -ForegroundColor Red
