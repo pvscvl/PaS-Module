4f3d05c2ef734622d33f89cbb8e7557f01645d81
pvscvl <pascalschoofs@icloud.com>
2024-02-27 03:44:36 +0100

    .

---
+++ PaS-Module/Functions/_510_Add-LocalAdmin.ps1
@@ -0,0 +1,36 @@
+function Add-LocalAdmin {
+	<#
+	.SYNOPSIS
+	Adds User to Administrators Group on Remote Device
+	
+	.DESCRIPTION
+	bla
+	
+	.EXAMPLE
+	$computerName = "REMOTE_COMPUTER_NAME"
+	$usernameToAdd = "USERNAME_TO_ADD"
+	Add-LocalAdmin -comp $computerName -sam $usernameToAdd
+#>
+	param (
+		[string]$ComputerName,
+		[string]$UserName,
+		[string]$domain = $env:USERDOMAIN
+	)
+
+	$Credential = Import-Clixml -Path "C:\Users\Pascal\tkm.cred"
+
+	if ($ComputerName -match '^\s*$') {
+		throw "Computer name not found: '$ComputerName'"
+	}
+
+	if ($UserName -match '^\s*$') {
+		throw "SAM not acceptable: '$UserName'"
+	}
+
+	$null = Invoke-Command -ComputerName $ComputerName -Credential $Credential -ScriptBlock {
+		param ($UserName, $domain)
+		$group = [ADSI]"WinNT://./Administratoren"
+		$user = [ADSI]"WinNT://$domain/$UserName"
+		$group.Add($user.Path)
+	} -ArgumentList $UserName, $domain
+}
