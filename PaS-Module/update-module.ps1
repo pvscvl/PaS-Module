fa74ee40dfc936c84757fbf26215e88e8ea43749
pvscvl <pascalschoofs@icloud.com>
2024-02-27 02:56:49 +0100

    .

--- PaS-Module/update-module.ps1
+++ PaS-Module/update-module.ps1
@@ -1,7 +1,11 @@
 $SCRIPTDIRECTORY = Split-Path -Parent $MyInvocation.MyCommand.Path
+Write-Host ""
+Write-Host ""
+Write-Host "Script Directory:`t$SCRIPTDIRECTORY"
 $FUNCTIONSFOLDER = Join-Path -Path $SCRIPTDIRECTORY -ChildPath "Functions"
+Write-Host "Function Folder:`t$FUNCTIONSFOLDER"
 $MODULEFILE = Join-Path -Path $SCRIPTDIRECTORY -ChildPath "PaS-Module.psm1"
-
+Write-Host "Module-File:`t`t$MODULEFILE"
 	$scriptFiles = Get-ChildItem -Path $FUNCTIONSFOLDER -Filter "*.ps1"
 	$moduleContent = ""
 
@@ -10,3 +14,7 @@ $MODULEFILE = Join-Path -Path $SCRIPTDIRECTORY -ChildPath "PaS-Module.psm1"
 		$moduleContent += $content + [Environment]::NewLine + [Environment]::NewLine
 	}
 $moduleContent | Out-File -FilePath $MODULEFILE -Force
+Write-Host ""
+Write-Host "Done.`t`t$MODULEFILE updated"
+Write-Host ""
+Write-Host ""
