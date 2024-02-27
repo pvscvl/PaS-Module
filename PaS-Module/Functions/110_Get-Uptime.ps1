1fc4d5cc7eab0579c2ed910111dd3a8484011f99
pvscvl <pascalschoofs@icloud.com>
2024-02-27 05:50:36 +0100

    Create 110_Get-Uptime.ps1

---
+++ PaS-Module/Functions/110_Get-Uptime.ps1
@@ -0,0 +1,17 @@
+function Get-Uptime {
+	param (
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		[string]$Computer
+	)
+	
+	if (-Not (Test-WinRMStatus -Computer $Computer)) {
+		Write-Host "$Computer`tN/A"
+		return
+	}
+	
+	$LastBootUpTime = (Get-CimInstance -ComputerName $Computer -ClassName Win32_OperatingSystem).LastBootUpTime
+	$Uptime = New-TimeSpan -Start $LastBootUpTime -End (Get-Date)
+	Write-Host "$Computer - System Uptime:`t $($Uptime.Days) days $($Uptime.Hours) hours $($Uptime.Minutes) minutes"
+}
+
