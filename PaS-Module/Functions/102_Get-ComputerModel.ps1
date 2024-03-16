7860908e05c47fd3fab738f8facf932753cc5c42
pvscvl <pascalschoofs@icloud.com>
2024-03-16 03:42:12 +0100

    Update 102_Get-ComputerModel.ps1

--- PaS-Module/Functions/102_Get-ComputerModel.ps1
+++ PaS-Module/Functions/102_Get-ComputerModel.ps1
@@ -6,10 +6,12 @@ function Get-ComputerModel{
 	)
 
 	if (-Not (Test-WinRMStatus -Computer $Computer)) {
+ 		$Computer = $Computer.ToUpper()
 		Write-Host "$Computer`tN/A"
 		return
 	}
 
+	$Computer = $Computer.ToUpper()
 	Write-Host -NoNewline "$Computer`tModel: "
 	(Get-CimInstance -ComputerName $Computer -ClassName Win32_ComputerSystem).Model
 }
