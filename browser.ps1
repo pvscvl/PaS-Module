c47cb40205fab1dd83b9cc7a75864f0c1accf9db
pvscvl <pascalschoofs@icloud.com>
2024-11-08 19:56:12 +0100

    Nicht hallo, nummer 2

--- browser.ps1
+++ browser.ps1
@@ -1,7 +1,7 @@
 Get-Content C:\temp\assoc.xml | Select-String -Pattern "Firefox|.xml|DefaultAssociations" | Out-File -Encoding utf8 C:\temp\browser.xml
 
 
- 
+ nicht hallo
 #  Get-Content .\assoc.xml |
 #  Select-String -Pattern "Chrome|.xml|DefaultAssociations"|
 #  Out-File -Encoding utf8 .\browser.xml
