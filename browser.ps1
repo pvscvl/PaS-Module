9a910c8f3e1e222a6a1c2c403bf53166806a0299
pvscvl <pascalschoofs@icloud.com>
2024-11-08 19:57:14 +0100

    1

--- browser.ps1
+++ browser.ps1
@@ -1,6 +1,5 @@
 Get-Content C:\temp\assoc.xml | Select-String -Pattern "Firefox|.xml|DefaultAssociations" | Out-File -Encoding utf8 C:\temp\browser.xml
 
-
  nicht hallo
 #  Get-Content .\assoc.xml |
 #  Select-String -Pattern "Chrome|.xml|DefaultAssociations"|
