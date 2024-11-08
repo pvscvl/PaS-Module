0c49af6cb1d84e09947c760513ee10ee5763dd59
pvscvl <pascalschoofs@icloud.com>
2024-11-08 19:34:57 +0100

    commit message.

--- browser.ps1
+++ browser.ps1
@@ -1,6 +1,6 @@
 Get-Content C:\temp\assoc.xml | Select-String -Pattern "Firefox|.xml|DefaultAssociations" | Out-File -Encoding utf8 C:\temp\browser.xml
 
-
+#
 
 #  Get-Content .\assoc.xml |
 #  Select-String -Pattern "Chrome|.xml|DefaultAssociations"|
